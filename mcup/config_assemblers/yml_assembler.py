d3c3a1296834a2b043c4da25b9d12dbb3efc3044
Kacper Jarosławski <kacperpolska8920@gmail.com>
2025-04-18 13:57:02 +0200

    Refactor YmlAssembler to clean unused configuration data

--- mcup/config_assemblers/yml_assembler.py
+++ mcup/config_assemblers/yml_assembler.py
@@ -7,12 +7,31 @@ from mcup.configs import ConfigFile
 class YmlAssembler(Assembler):
     """Class representing yaml configuration files assembler."""
     @staticmethod
+    def clean_config(data):
+        if isinstance(data, dict):
+            cleaned_dict = {
+                k: YmlAssembler.clean_config(v)
+                for k, v in data.items()
+                if v is not None
+            }
+            return {k: v for k, v in cleaned_dict.items() if v not in (None, {}, [])}
+        elif isinstance(data, list):
+            cleaned_list = [
+                YmlAssembler.clean_config(item)
+                for item in data
+                if item is not None
+            ]
+            return [item for item in cleaned_list if item not in (None, {}, [])]
+        return data
+
+    @staticmethod
     def assemble(path: str, config: ConfigFile):
         with open(f"{path}/{config.config_file_path}/{config.config_file_name}", "w") as config_file:
-            filtered_config = {key: value for key, value in config.get_configuration().items() if value is not None}
+            filtered_config = YmlAssembler.clean_config(config.get_configuration())
             yaml.dump(
                 filtered_config,
                 config_file,
                 sort_keys=False,
-                default_flow_style=False
+                default_flow_style=False,
+                default_style=None
             )
