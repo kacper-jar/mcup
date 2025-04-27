32926eb9712839555fd161f4b37734eae6d39e96
Kacper Jarosławski <kacperpolska8920@gmail.com>
2025-04-27 18:06:26 +0200

    Refactor CollectorInputType to use enum.Enum and auto values

--- mcup/utils/ui/collector/collector_input_type.py
+++ mcup/utils/ui/collector/collector_input_type.py
@@ -1,7 +1,9 @@
-class CollectorInputType:
-    STRING = 1
-    INT = 2
-    BOOL = 3
-    STRING_LIST = 4
-    INT_LIST = 5
-    BOOL_LIST = 6
+import enum
+
+class CollectorInputType(enum.Enum):
+    STRING = enum.auto()
+    INT = enum.auto()
+    BOOL = enum.auto()
+    STRING_LIST = enum.auto()
+    INT_LIST = enum.auto()
+    BOOL_LIST = enum.auto()
