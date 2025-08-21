8ee02aa354234cde7e13ce81762d863524049124
Kacper Jarosławski <kacper.jaroslawski@kzl21.ovh>
2025-08-21 23:43:53 +0200

    fix: patch issue causing advanced fields to not defaults when in basic mode

--- mcup/cli/ui/components/server_configs_collector.py
+++ mcup/cli/ui/components/server_configs_collector.py
@@ -6,6 +6,8 @@ from mcup.core.configs import ServerPropertiesConfig, BukkitConfig, SpigotConfig
     PaperWorldDefaultsConfig, BashStartScript, BatchStartScript
 from mcup.cli.ui.components import ServerPropertiesCollector, BukkitCollector, SpigotCollector, PaperCollector, \
     StartScriptCollector, PaperGlobalCollector, PaperWorldDefaultsCollector, ServerConfigsCollectorFlags
+from mcup.core.status import StatusCode
+from mcup.core.user_config import UserConfig
 from mcup.core.utils.version import Version
 
 
@@ -14,6 +16,7 @@ class ServerConfigsCollector:
     def collect_configurations(server_version, configs,
                                flags=ServerConfigsCollectorFlags.NONE) -> AssemblerLinkerConfig:
         language = Language()
+        user_config = UserConfig()
 
         version = Version.from_string(server_version)
 
@@ -26,10 +29,23 @@ class ServerConfigsCollector:
 
         no_defaults = flags == ServerConfigsCollectorFlags.NO_DEFAULTS
 
+        for status in user_config.get_configuration("advancedmode.enabled", default="false"):
+            if status.status_code == StatusCode.SUCCESS:
+                advanced_mode_enabled = str(status.status_details).lower() == "true"
+                break
+            else:
+                advanced_mode_enabled = False
+                break
+
+        if not advanced_mode_enabled:
+            print(language.get_string("INFO_ADVANCED_MODE_DISABLED"))
+
         server_properties = ServerPropertiesConfig()
         if flags != ServerConfigsCollectorFlags.ALL_DEFAULTS:
+            if not advanced_mode_enabled:
+                server_properties.set_configuration_default_all_properties(version)
             collector = ServerPropertiesCollector()
-            output = collector.start_collector(version, no_defaults)
+            output = collector.start_collector(version, no_defaults, advanced_mode_enabled)
             server_properties.set_configuration_properties(output, version)
         else:
             server_properties.set_configuration_default_all_properties(version)
@@ -38,8 +54,10 @@ class ServerConfigsCollector:
         if "bukkit" in configs:
             bukkit_config = BukkitConfig()
             if flags != ServerConfigsCollectorFlags.ALL_DEFAULTS:
+                if not advanced_mode_enabled:
+                    bukkit_config.set_configuration_default_all_properties(version)
                 bukkit_collector = BukkitCollector()
-                output = bukkit_collector.start_collector(version, no_defaults)
+                output = bukkit_collector.start_collector(version, no_defaults, advanced_mode_enabled)
                 bukkit_config.set_configuration_properties(output, version)
             else:
                 bukkit_config.set_configuration_default_all_properties(version)
@@ -49,8 +67,10 @@ class ServerConfigsCollector:
         if "spigot" in configs:
             spigot_config = SpigotConfig()
             if flags != ServerConfigsCollectorFlags.ALL_DEFAULTS:
+                if not advanced_mode_enabled:
+                    spigot_config.set_configuration_default_all_properties(version)
                 spigot_collector = SpigotCollector()
-                output = spigot_collector.start_collector(version, no_defaults)
+                output = spigot_collector.start_collector(version, no_defaults, advanced_mode_enabled)
                 spigot_config.set_configuration_properties(output, version)
             else:
                 spigot_config.set_configuration_default_all_properties(version)
@@ -60,8 +80,10 @@ class ServerConfigsCollector:
         if "paper" in configs:
             paper_config = PaperConfig()
             if flags != ServerConfigsCollectorFlags.ALL_DEFAULTS:
+                if not advanced_mode_enabled:
+                    paper_config.set_configuration_default_all_properties(version)
                 paper_collector = PaperCollector()
-                output = paper_collector.start_collector(version, no_defaults)
+                output = paper_collector.start_collector(version, no_defaults, advanced_mode_enabled)
                 paper_config.set_configuration_properties(output, version)
             else:
                 paper_config.set_configuration_default_all_properties(version)
@@ -71,9 +93,11 @@ class ServerConfigsCollector:
         if "paper-global" in configs:
             paper_global_config = PaperGlobalConfig()
             if flags != ServerConfigsCollectorFlags.ALL_DEFAULTS:
+                if not advanced_mode_enabled:
+                    paper_global_config.set_configuration_default_all_properties(version)
                 paper_global_config.set_configuration_default_property("_version", version)
                 paper_global_config_collector = PaperGlobalCollector()
-                output = paper_global_config_collector.start_collector(version, no_defaults)
+                output = paper_global_config_collector.start_collector(version, no_defaults, advanced_mode_enabled)
                 paper_global_config.set_configuration_properties(output, version)
             else:
                 paper_global_config.set_configuration_default_all_properties(version)
@@ -83,9 +107,12 @@ class ServerConfigsCollector:
         if "paper-world-defaults" in configs:
             paper_world_defaults_config = PaperWorldDefaultsConfig()
             if flags != ServerConfigsCollectorFlags.ALL_DEFAULTS:
+                if not advanced_mode_enabled:
+                    paper_world_defaults_config.set_configuration_default_all_properties(version)
                 paper_world_defaults_config.set_configuration_default_property("_version", version)
                 paper_world_defaults_config_collector = PaperWorldDefaultsCollector()
-                output = paper_world_defaults_config_collector.start_collector(version, no_defaults)
+                output = paper_world_defaults_config_collector.start_collector(version, no_defaults,
+                                                                               advanced_mode_enabled)
                 paper_world_defaults_config.set_configuration_properties(output, version)
             else:
                 paper_world_defaults_config.set_configuration_default_all_properties(version)
@@ -104,8 +131,10 @@ class ServerConfigsCollector:
                 start_script_config = BashStartScript()
 
             if flags != ServerConfigsCollectorFlags.ALL_DEFAULTS:
+                if not advanced_mode_enabled:
+                    start_script_config.set_configuration_default_all_properties(version)
                 start_script_collector = StartScriptCollector()
-                output = start_script_collector.start_collector(version, no_defaults)
+                output = start_script_collector.start_collector(version, no_defaults, advanced_mode_enabled)
                 start_script_config.set_configuration_properties(output, version)
             else:
                 start_script_config.set_configuration_default_all_properties(version)
