f3c1773ef9760131ce4b74d1e15d2197a3100c96
Kacper Jarosławski <kacper.jaroslawski@kzl21.ovh>
2025-09-06 13:11:44 +0200

    refactor: make all options in start script collector use basic input mode

--- mcup/cli/ui/components/start_script_collector.py
+++ mcup/cli/ui/components/start_script_collector.py
@@ -11,13 +11,13 @@ class StartScriptCollector(Collector):
             "Configuration",
             [
                 CollectorInput("screen-name", "COLLECTOR_START_SCRIPT_SCREEN_NAME", CollectorInputType.STRING,
-                               CollectorInputMode.ADVANCED, OLDEST_SUPPORTED_VERSION, LATEST_VERSION),
+                               CollectorInputMode.BASIC, OLDEST_SUPPORTED_VERSION, LATEST_VERSION),
                 CollectorInput("initial-heap", "COLLECTOR_START_SCRIPT_INITIAL_HEAP", CollectorInputType.INT,
                                CollectorInputMode.BASIC, OLDEST_SUPPORTED_VERSION, LATEST_VERSION),
                 CollectorInput("max-heap", "COLLECTOR_START_SCRIPT_MAX_HEAP", CollectorInputType.INT,
                                CollectorInputMode.BASIC, OLDEST_SUPPORTED_VERSION, LATEST_VERSION),
                 CollectorInput("use-aikars-flags", "COLLECTOR_START_SCRIPT_USE_AIKARS_FLAGS", CollectorInputType.BOOL,
-                               CollectorInputMode.ADVANCED, OLDEST_SUPPORTED_VERSION, LATEST_VERSION),
+                               CollectorInputMode.BASIC, OLDEST_SUPPORTED_VERSION, LATEST_VERSION),
             ]
         ))
         self.add_section(CollectorSection(
@@ -27,7 +27,7 @@ class StartScriptCollector(Collector):
                                CollectorInputType.INT, CollectorInputMode.BASIC, OLDEST_SUPPORTED_VERSION,
                                LATEST_VERSION),
                 CollectorInput("restart-delay", "COLLECTOR_START_SCRIPT_RESTART_DELAY",
-                               CollectorInputType.INT, CollectorInputMode.ADVANCED, OLDEST_SUPPORTED_VERSION,
+                               CollectorInputType.INT, CollectorInputMode.BASIC, OLDEST_SUPPORTED_VERSION,
                                LATEST_VERSION),
             ]
         ))
