2cb8e1a26abb0ab4cc7a3e1aa342e9304faef226
Kacper Jarosławski <kacper.jaroslawski@kzl21.ovh>
2026-01-25 23:20:46 +0100

    test: add unit tests for `EulaAssembler` class

---
+++ tests/core/config_assemblers/test_eula_assembler.py
@@ -0,0 +1,23 @@
+import pytest
+from unittest.mock import MagicMock, patch, mock_open
+from mcup.core.config_assemblers.eula_assembler import EulaAssembler
+
+
+class TestEulaAssembler:
+
+    def test_assemble(self):
+        mock_config = MagicMock()
+        mock_config.config_file_path = "."
+        mock_config.config_file_name = "eula.txt"
+
+        with patch("builtins.open", mock_open()) as mock_file:
+            EulaAssembler.assemble("/server/path", mock_config)
+
+            mock_file.assert_called_with("/server/path/./eula.txt", "w")
+
+            handle = mock_file()
+            args, _ = handle.write.call_args
+            content = args[0]
+
+            assert "eula=true" in content
+            assert "https://aka.ms/MinecraftEULA" in content
