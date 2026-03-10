36e728316dbcff870015d6b5199e3d4c6bdb93a7
Kacper Jarosławski <kacper.jaroslawski@kzl21.ovh>
2026-03-10 19:40:35 +0100

    feat: add support for customizable feature-seeds in paper world defaults collector

--- mcup/cli/ui/elements/collector/collector_input_type.py
+++ mcup/cli/ui/elements/collector/collector_input_type.py
@@ -21,3 +21,4 @@ class CollectorInputType(enum.Enum):
     PAPER_ALT_ITEM_DESPAWN_RATE_ITEM_TYPE = enum.auto()
     PAPER_DESPAWN_RANGES_MOB_CATEGORY = enum.auto()
     PAPER_DESPAWN_TIME_ENTITY_TYPE = enum.auto()
+    PAPER_FEATURE_SEEDS_FEATURE_NAMESPACE = enum.auto()
