9c14d710060b04310777416bee4c6f2a9f797cb9
Kacper Jarosławski <kacper.jaroslawski@kzl21.ovh>
2026-03-11 16:50:45 +0100

    fix: expose `NestedCollector` in module exports

--- mcup/cli/ui/elements/collector/__init__.py
+++ mcup/cli/ui/elements/collector/__init__.py
@@ -3,5 +3,6 @@ from .collector_section import CollectorSection
 from .collector_input import CollectorInput
 from .collector_input_type import CollectorInputType
 from .collector_input_mode import CollectorInputMode
+from .nested_collector import NestedCollector
 
-__all__ = ["Collector", "CollectorSection", "CollectorInput", "CollectorInputType", "CollectorInputMode"]
+__all__ = ["Collector", "CollectorSection", "CollectorInput", "CollectorInputType", "CollectorInputMode", "NestedCollector"]
